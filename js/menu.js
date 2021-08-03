c82d0b96c0b96d67e78145873607307f04428a59
codecaiine <akabrouyannickn@gmail.com>
2021-08-03 16:31:20 +0000

    Fix linters js errors

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,12 @@ const weCloseMenu = document.querySelector('.closeMenu');
 const weOpenMenu = document.querySelector('.openMenu');
 
 function show() {
-    myTopMenu.style.display = 'flex';
-    myTopMenu.style.top = '0';
+  myTopMenu.style.display = 'flex';
+  myTopMenu.style.top = '0';
 }
 
 function close() {
-    myTopMenu.style.top = '-110%';
+  myTopMenu.style.top = '-110%';
 }
 
 weOpenMenu.addEventListener('click', show);
